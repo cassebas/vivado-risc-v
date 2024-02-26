49e7e2ce2ef133467de3e18e64cac12b178951d9
Caspar Treijtel <18738369+cassebas@users.noreply.github.com>
2024-02-26 14:09:02 +0100

    Minor: improve output of running single benchmark

--- run-single-benchmark.tcl
+++ run-single-benchmark.tcl
@@ -1,11 +1,13 @@
 set benchmark_elf [lindex ${argv} 0]
 set benchmark_time [lindex ${argv} 1]
-puts "Downloading benchmark binary ${benchmark_elf} for running benchmark."
-puts "Using a delay of ${benchmark_time} seconds after starting benchmark."
+puts "run-single-benchmark.tcl: downloading benchmark binary ${benchmark_elf} for running benchmark."
+puts "run-single-benchmark.tcl: using a delay of ${benchmark_time} seconds after starting benchmark."
 connect
 targets -set -filter {name =~ "Hart #0*"}
 stop
 dow -clear ${benchmark_elf}
+puts "run-single-benchmark.tcl: starting benchmark now."
 con
 # Wait a while for benchmark to properly finish execution
+puts "run-single-benchmark.tcl: waiting for ${benchmark_time} seconds."
 after [expr ${benchmark_time} * 1000]
